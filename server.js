435e099bc9d67cb5bb1a2765bf52f2f1d658785a
muratcanoncu <murat.oncu1992@hotmail.com>
2021-04-29 14:29:52 +0200

    setup second

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const app = express.Router();
+
+app.set(express.static(`${__dirname}/public`));
+
+app.listen(5000, () => {
+  console.log("Server is running on PORT:" + 5000);
+});
