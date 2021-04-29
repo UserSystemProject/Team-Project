f7391fa9d39785749bcea97597832e7b5896a6f5
muratcanoncu <murat.oncu1992@hotmail.com>
2021-04-29 15:06:54 +0200

    user/product Schemas created

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const app = express.Router();
+const app = express();
+require("dotenv").config();
+const PORT = 5000;
+//! Setting
+app.use(express.static(`${__dirname}/public`));
+app.set("view engine", "hbs");
+app.use(express.urlencoded({ extended: false }));
 
-app.set(express.static(`${__dirname}/public`));
+//! Database connection
+const mongoose = require("mongoose");
+const dataBaseName = process.env.dataBaseHiddenName;
+const dataBaseLink = process.env.dataBaseHiddenLink + dataBaseName;
+mongoose
+  .connect(dataBaseLink, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(() => console.log("DataBase connection is failed"));
 
-app.listen(5000, () => {
-  console.log("Server is running on PORT:" + 5000);
+//! Listen
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
 });
