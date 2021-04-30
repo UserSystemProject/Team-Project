9af3166b20cd684f7de315191b9221b1210896ac
muratcanoncu <murat.oncu1992@hotmail.com>
2021-04-30 14:58:05 +0200

    updated

--- server.js
+++ server.js
@@ -24,45 +24,44 @@ mongoose
 //! Adding models
 const User = require("./models/User");
 const Product = require("./models/Product");
-const { Router } = require("express");
-
 //! Home
 app.get("/", (req, res) => {
   res.render("home", { pageTitle: "Home Page" });
 });
-
-//! Login (Read of cRud)
-app.get("/login", (req, res) => {
-  // User.find((err, info) => {
-  //   const character = req.query;
-  res.render("login");
-  // });
-});
-app.post("/login", (req, res) => {
-  // we need to use find method here
-  // User.find((err, users) => {
-  //   console.log(users);
-  // });
-  res.redirect("/product");
-});
-
-//! Product user
-
-app.get("/product", (req, res) => {
+//! product admin
+// r
+app.get("/productadmin", (req, res) => {
   Product.find((err, product) => {
-    res.render("product", { product });
+    User.find((err, user) => {
+      console.log(user, product);
+      res.render("productadmin", { products: product, users: user });
+    });
   });
 });
-
-app.post("/product", (req, res) => {
+// c
+app.post("/productadmin", (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save().then(() => {
+    res.redirect("/productadmin");
+  });
+});
+//! Product User
+// r
+app.get("/productuser", (req, res) => {
+  Product.find((err, product) => {
+    User.find((err, user) => {
+      res.render("productuser", { product, user });
+    });
+  });
+});
+// c
+app.post("/productuser", (req, res) => {
   const newProduct = new Product(req.body);
   newProduct.save().then(() => {
-    res.redirect("/product");
+    res.redirect("/productuser");
   });
 });
-
-// updating manually
-
+//! updating manually
 app.get("product/update/:id", (req, res) => {
   const updateProduct = req.params.id;
   Product.findByIdAndUpdate(
@@ -70,6 +69,35 @@ app.get("product/update/:id", (req, res) => {
     { title: "updating" },
     (err, doc) => {
       res.redirect("/product");
+    }
+  );
+});
+//! Login (Read of cRud)
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", (req, res) => {
+  User.findOne(
+    { email: req.body.email, password: req.body.password },
+    (err, user) => {
+      console.log(user);
+      if (user == null) {
+        res.redirect(
+          url.format({
+            pathname: "/login",
+            query: {
+              message: "E-mail or Password is wrong, please check it!",
+              falseEntered: false,
+            },
+          })
+        );
+      } else if (!Object.keys(user) == 0) {
+        if (user.role == "administrator") {
+          res.redirect("/productadmin");
+        } else {
+          res.redirect("/productuser");
+        }
+      }
     }
   );
 });
@@ -85,15 +113,7 @@ app.post("/register", (req, res) => {
     res.redirect("/login");
   });
 });
-//Reset Password
-app.get("/resetPassword", (req, res) => {
-  res.render("resetPassword");
-});
-app.post("/resetPassword", (req, res) => {
-  console.log("Data is received from user:", req.body);
-  // res.end(JSON.stringify(req.body));
-});
-//404 error
+//! 404 error
 app.get("*", (req, res) => {
   res.render("404error");
 });
