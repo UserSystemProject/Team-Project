647a47fc5ca46e515c3c4e9094adf5123384ce74
Nicolas Alvarez <neao002@gmail.com>
2021-05-01 20:21:53 +0200

    adding

--- server.js
+++ server.js
@@ -23,17 +23,16 @@ app.get("/", (req, res) => {
 });
 //! product admin
 // r
-app.get("/login/productadmin", (req, res) => {
-  const userName = req.query.userName;
+app.get("/productadmin", (req, res) => {
   Product.find((err, product) => {
     User.find((err, user) => {
-      // console.log(user, product);
-      res.render("productadmin", { products: product, users: user, userName });
+      console.log(user, product);
+      res.render("productadmin", { products: product, users: user });
     });
   });
 });
 // c
-app.post("/login/productadmin", (req, res) => {
+app.post("/productadmin", (req, res) => {
   const newUser = new User(req.body);
   newUser.save().then(() => {
     res.redirect("/productadmin");
@@ -41,17 +40,15 @@ app.post("/login/productadmin", (req, res) => {
 });
 //! Product User
 // r
-app.get("/login/productuser", (req, res) => {
-  console.log(req.query.userName);
-  const userName = req.query.userName;
+app.get("/productuser", (req, res) => {
   Product.find((err, product) => {
     User.find((err, user) => {
-      res.render("productuser", { product, user, userName });
+      res.render("productuser", { product, user });
     });
   });
 });
 // c
-app.post("/login/productuser", (req, res) => {
+app.post("/productuser", (req, res) => {
   const newProduct = new Product(req.body);
   newProduct.save().then(() => {
     res.redirect("/productuser");
@@ -59,14 +56,14 @@ app.post("/login/productuser", (req, res) => {
 });
 
 //! updating manually
-app.get("/login/product/update/:id", (req, res) => {
+app.get("productuser/update/:id", (req, res) => {
   const updateProduct = req.params.id;
   Product.findByIdAndUpdate(
     updateProduct,
     { title: "updating" },
     (err, doc) => {
-      console.log("Product updated:", doc);
-      res.redirect("/product");
+      console.log("updated", doc);
+      res.redirect("/productuser");
     }
   );
 });
@@ -93,21 +90,9 @@ app.post("/login", (req, res) => {
         );
       } else if (!Object.keys(user) == 0) {
         if (user.role == "administrator") {
-          console.log(user.name);
-          res.redirect(
-            url.format({
-              pathname: "/login/productadmin",
-              query: { userName: user.name },
-            })
-          );
+          res.redirect("/productadmin");
         } else {
-          console.log(user.name);
-          res.redirect(
-            url.format({
-              pathname: "/login/productuser",
-              query: { userName: user.name },
-            })
-          );
+          res.redirect("/productuser");
         }
       }
     }
@@ -126,10 +111,10 @@ app.post("/register", (req, res) => {
   });
 });
 
-//! 404 error
-app.get("*", (req, res) => {
-  res.render("404error");
-});
+// 404 error
+//app.get("*", (req, res) => {
+// res.render("404error");
+// });
 
 //! Listen
 app.listen(PORT, () => {
