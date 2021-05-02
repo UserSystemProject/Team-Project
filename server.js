d2deaba0d8b5f8fc734e935c2af551458ac451d9
muratcanoncu <murat.oncu1992@hotmail.com>
2021-05-02 20:58:40 +0200

    homepage styling

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+// require("dotenv").config();
 const PORT = 5000;
-
 //! Mongoose Connection
 const connectDB = require("./Config/db");
 connectDB();
@@ -10,19 +9,16 @@ connectDB();
 app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: false }));
-
 //! Routes
 const indexRouter = require("./routes/indexRouter");
 const signUpRouter = require("./routes/SingUpRouter");
 const loginRouter = require("./routes/loginRouter");
-
 //? Home
 app.use("/", indexRouter);
 //? Register
 app.use("/register", signUpRouter);
 //? Login
 app.use("/login", loginRouter);
-
 //! 404 error
 app.get("*", (req, res) => {
   res.render("404error");
